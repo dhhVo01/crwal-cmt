47691fe57c6ae3bc7e485a7cfdae42acad3ecece
Dangvhh <vohuynhhaidang24032001@gmail.com>
2022-11-21 09:20:36 +0700

    fuck-2

--- tool.py
+++ tool.py
@@ -1,11 +1,11 @@
 import sys
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 import time
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 url = sys.argv[1]
 driver.get(url)
